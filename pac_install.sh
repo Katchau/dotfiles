096d99b1a7349c8a997e091167132355683d4b5f
Diogo Campos <d.diogocampos@gmail.com>
2017-03-15 11:07:42 +0000

    Added programs to install

--- pac_install.sh
+++ pac_install.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-sudo pacman -Sy --noconfirm yaourt zsh
+sudo pacman -Sy --noconfirm yaourt zsh vim wget curl
 yaourt -Sy --noconfirm atom google-chrome rambox-bin neofetch paper-icon-theme atom jdk
